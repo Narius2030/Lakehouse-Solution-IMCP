38c78812a4b5feb24821606cb109fd7cb8fcebdf
Narius2030 <buiducnhan15122003@gmail.com>
2025-02-09 19:25:20 +0700

    feat: create streaming for MinIO events when upload files

--- notebook/work/utils/streaming_functions.py
+++ notebook/work/utils/streaming_functions.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append("./work/imcp")
 
 import pyspark.sql.functions as F
+import pyspark.sql.types as T
 from utils.schema import csv_sample_schema
 
 def process_stream(stream):
@@ -13,10 +14,30 @@ def process_stream(stream):
              )
     return stream
 
+def read_stopwords():
+    # Đọc stopwords từ file
+    with open("./data/stopwords.txt", "r", encoding="utf-8") as file:
+        custom_stopwords = list(line.strip() for line in file)
+    return custom_stopwords
+
+def remove_stopwords(words):
+    stopwords = read_stopwords()
+    words = [word for word in words if word not in stopwords]
+    return words
+
 def normalize_caption(df_file, column):
     df_file = df_file.drop("short_caption").dropDuplicates()  # Xử lý làm sạch data
-    df_cleaned = df_file.withColumn(column, F.regexp_replace(F.col(column), "[^a-zA-Z0-9\\s]", ""))
-    df_cleaned = df_cleaned.withColumn(f"{column}_tokens", F.split(F.col(column), " "))
+    df_cleaned = (
+                    df_file.withColumn(column, F.regexp_replace(F.col(column), "[^a-zA-Z0-9\\s]", ""))
+                        .withColumn(f"{column}_tokens", F.split(F.col(column), " "))
+                 )
+    
+    udf_remove_stopwords = F.udf(remove_stopwords, T.ArrayType(T.StringType()))
+    df_cleaned = (
+                    df_cleaned.withColumn("word_count", F.count(F.size(f"{column}_tokens")))
+                            .withColumn("short_caption_tokens", udf_remove_stopwords(F.col(f"{column}_tokens")))
+                 )
+    
     df_cleaned = df_cleaned.limit(10)
     return df_cleaned
 
